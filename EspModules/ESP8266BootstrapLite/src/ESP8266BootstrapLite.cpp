03728f93bba0f1b17fee730fb3297bba16420695
Prateek Gupta <guptaprateek328@gmail.com>
2018-10-14 14:41:02 -0700

    removed extra spaces from the code

--- EspModules/ESP8266BootstrapLite/src/ESP8266BootstrapLite.cpp
+++ EspModules/ESP8266BootstrapLite/src/ESP8266BootstrapLite.cpp
@@ -31,54 +31,30 @@ ESP8266BootstrapLite::~ESP8266BootstrapLite(void){}
 * This function initializes Serial, SPIFFS and Debug fcreds.
 */
 bool ESP8266BootstrapLite::begin(){
-
 	//ToDo : Check available size of SPIFFS
-	
 	//Serial.begin(115200);
-
-
 	DEBUG_PRINTLN("[INFO begin] Mounting SPIFFS\n");
-	
 	if(SPIFFS.begin()){
-
 		DEBUG_PRINTLN("[INFO begin] Checking if configs file exists.\n");
-		
 		if(SPIFFS.exists(BOOTLITE_SAVED_NETCONFIGS_FILE)){
-			
 			File f = SPIFFS.open(BOOTLITE_SAVED_NETCONFIGS_FILE, "r");
-			
 			if(f && f.size() > 0){
-
 				DEBUG_PRINTLN("[INFO begin] Wifi credentials are available. Will try to connect using these credentials.\n");
-
 				state = STATE_WIFI_CONNECT;
-
 				f.close();
-			
 			}else{
-			
 				DEBUG_PRINTLN("[INFO begin] No Wifi credentials are saved yet.\n");
-
 				state = STATE_ACCESS_POINT_CONNECT;
 			}
-		
 		}else{
-
 			DEBUG_PRINTLN("[INFO begin] Device booting for the first time. Creating required files to save wifi credentials.\n");
-			
 			File f = SPIFFS.open(BOOTLITE_SAVED_NETCONFIGS_FILE ,"w");
-
 			state = STATE_ACCESS_POINT_CONNECT;
-
 			f.close();
 		}
-
 		return true;
-
 	}else{
-
 		DEBUG_PRINTLN("[ERROR begin] SPIFFS did not mount.\n");
-	
 	}
 	return false;
 }
@@ -87,11 +63,8 @@ bool ESP8266BootstrapLite::begin(){
 * This function disconnects Serial, SPIFF, Debug and Wifi settings and prepare ESP for reboot.
 */
 void ESP8266BootstrapLite::end(bool reboot = false){
-
 	teardownWifi();
-
 	if(reboot){
-		
 		ESP.restart();
 	}
 }
@@ -100,69 +73,41 @@ void ESP8266BootstrapLite::end(bool reboot = false){
 * User should call this function inside the loop() function of Arduino. 
 */
 ESPBootstrapError ESP8266BootstrapLite::bootstrap(){
-
 	ESPBootstrapError err = NO_ERROR;
 
 	switch(state){
-
 		//If user forgets to call begin() in setup function, then this will execute begin().
 		case STATE_READY: 
-
 			begin();
-
 			DEBUG_PRINTLN("[INFO bootstrap] current state = STATE_READ\n");
-		break;
-		
+			break;
 		case STATE_WIFI_CONNECT: 
-
 			DEBUG_PRINTLN("[INFO bootstrap] current state = STATE_WIFI_CONNECT\n");
-			
 			err =  attemptConnectToNearbyWifi(); // ToDo: Create a loop to attempt 3 times if necessary.
-
 			if( err == ERROR_WIFI_CONNECT ) state = STATE_ACCESS_POINT_CONNECT;
-
 			//if( err == NO_ERROR ) state = STATE_WIFI_ACTIVE;
-		
-		break;
-
+			break;
 		case STATE_WIFI_ACTIVE:
-
 			DEBUG_PRINTLN("[INFO bootstrap] current state = STATE_WIFI_ACTIVE\n");
-
 			err = NO_ERROR;
-
-		break;
-		
+			break;
 		case STATE_ACCESS_POINT_CONNECT: 
-
 			//This will try to start Access Point of the device.
 			err = startSoftAP();
-
 			DEBUG_PRINTLN("[Info bootstrap] current state = STATE_ACCESS_POINT_CONNECT\n");
-		
-		break;
-		
+			break;
 		case STATE_ACCESS_POINT_ACTIVE:
-
 			DEBUG_PRINTLN("[Info bootstrap] current state = STATE_ACCESS_POINT_ACTIVE\n");
-			
 			while(state == STATE_ACCESS_POINT_ACTIVE){ 
-				
 				server->handleClient(); 
 			}
-		
-		break;
-		
+			break;
 		//This state is not used yet.
 		case STATE_SLEEP:
-
 			DEBUG_PRINTLN("[Info bootstrap] current state = STATE_SLEEP\n");
-
 			err = NO_ERROR;
-
-		break;
+			break;
 	}
-
 	return err;
 }
 
@@ -172,44 +117,31 @@ ESPBootstrapError ESP8266BootstrapLite::bootstrap(){
 * established otherwise it returns an error code so that the calling code can initiate access point.
 */
 ESPBootstrapError ESP8266BootstrapLite::attemptConnectToNearbyWifi(){
-	
 	DEBUG_PRINTLN("[INFO NearbyWifi] Attempting to connect to wifi in vicinity with credentials stored on ESP.\n");
-
 	//Shut down wifi with previous settings.
 	teardownWifi();
-
 	WiFi.mode(WIFI_STA);
-	
 	int numOfNetworks = WiFi.scanNetworks();
-
 	bool found = false;
 	String ssid = "\0";
 	String pass = "\0";
 
 	if(numOfNetworks == 0){
-		
 		DEBUG_PRINTLN("[INFO NearbyWifi] No networks found in the vicinity.\n");
-		
 		//state = STATE_ACCESS_POINT_CONNECT;
-		
 		return ERROR_WIFI_CONNECT;
 	}
 
 	DEBUG_PRINTLN("[INFO NearbyWifi] Networks found in the vicinity.\n");
-
 	File fcreds = SPIFFS.open(BOOTLITE_SAVED_NETCONFIGS_FILE, "r");
 
 	if(!fcreds){
-		
 		DEBUG_PRINTLN("[ERROR NearbyWifi] Failed to open saved networks files.\n");
-		
 		//state = STATE_ACCESS_POINT_CONNECT;
-		
 		return ERROR_WIFI_CONNECT;
 	}
 
 	while(fcreds.available()){
-
 		//ssid and password are stored in separate lines to avoid complexity of the program.
 		ssid = fcreds.readStringUntil('\n');
 		pass = fcreds.readStringUntil('\n');
@@ -220,42 +152,30 @@ ESPBootstrapError ESP8266BootstrapLite::attemptConnectToNearbyWifi(){
 		DEBUG_PRINTLN("Length of ssid = %d", ssid.length());
 
 		for(int i = 0 ; i < numOfNetworks; i++){
-
 			DEBUG_PRINTLN("Checking if saved_ssid : %s == ssid : %s. length = %d.\n", &ssid[0], &((WiFi.SSID(i))[0]), WiFi.SSID(i).length() );
-
 			if(ssid == WiFi.SSID(i)){
 				found = true;
 				break;
 			}
 		}
-
 		if(found) break;
 	}
 
 	if(found){
-		
 		DEBUG_PRINTLN("[INFO NearbyWifi] Trying to connect to SSID = %s .\n", &ssid[0]);
-		
 		return connectToWifi(ssid, pass); // Beware, need to check if the string contains "\n" at the end
 	}
 	
 	DEBUG_PRINTLN("[INFO NearbyWifi] No match found in stored networks.\n");
-	
 	//state = STATE_ACCESS_POINT_CONNECT;
-	
 	return ERROR_WIFI_CONNECT;
-
 }
 
 
 void ESP8266BootstrapLite::storeUserTokenInSPIFFS(String token) const {
-
 	if(SPIFFS.begin()){
-
 		File file = SPIFFS.open(BOOTLITE_USER_TOKEN_FILE, "w");
-
 		if(file){
-			
 			file.println(token);
 			file.close();
 		}
@@ -265,16 +185,12 @@ void ESP8266BootstrapLite::storeUserTokenInSPIFFS(String token) const {
 }
 
 String ESP8266BootstrapLite::getUserTokenFromSPIFFS(){
-
 	String token;
-
 	File file = SPIFFS.open(BOOTLITE_USER_TOKEN_FILE, "r");
-
 	if(file){
 		token = file.readStringUntil('\n');
 		file.close();
 	}
-
 	return token;
 }
 
@@ -283,53 +199,32 @@ String ESP8266BootstrapLite::getUserTokenFromSPIFFS(){
 *  Handler method that receives the Wifi credentials over the Access Point server and stores them in SPIFFS for future use.
 */
 void ESP8266BootstrapLite::handleConfig(){
-
 	String ssid, password, token;
 
 	if(server->hasArg("token")){
-
 		token = server->arg("token");
-
 		storeUserTokenInSPIFFS(token);
-
 		//_user_token = token;
 	}
 
-		//if(token == _ap_token) {
-	
 	if(server->hasArg("ssid") && server->hasArg("password")){
-	    
 	    ssid = server->arg("ssid");
-	    
 	    password = server->arg("password");
-	    
 	    DEBUG_PRINTLN("[INFO ap_handler]Received Args");
-
 	    server->send(200, "text/plain", "Got configuration params");
-
 	    delay(BOOTLITE_DELAY_TIME);
-
 	    ESPBootstrapError err = connectToWifi(ssid, password);
 
 	    if(err == NO_ERROR){
-
 	    	storeWifiConfInSPIFF(ssid, password);
-
 	    } else {
-
 	    	DEBUG_PRINTLN("[ERROR ap_handler] Failed to connect to Wifi with ssid %s.\n", &ssid[0]);
-
 	    	state = STATE_ACCESS_POINT_CONNECT;
 	    }
-
 	 } else {
-
 	 	DEBUG_PRINTLN("[ERROR ap_handler] ssid or password field not found.\n");
-
 	 	state = STATE_ACCESS_POINT_CONNECT;
-
 	 	server->send(400, "text/plain", "Bad request");
-
 	 }
 }
 
@@ -350,11 +245,8 @@ void ESP8266BootstrapLite::handleNotFound(){
   message += "\n";
 
   for( uint8_t i = 0; i < server->args(); i++ ) {
-
     message += " " + server->argName ( i ) + ": " + server->arg ( i ) + "\n";
-
   }
-
   server->send ( 404, "text/plain", message );
 }
 
@@ -362,27 +254,16 @@ void ESP8266BootstrapLite::handleNotFound(){
 * Starts access point mode on the device, start a server, and registers the handlers for wifi cerdentials and invalid resource accesses.
 */
 ESPBootstrapError ESP8266BootstrapLite::startSoftAP() {
-
 	DEBUG_PRINTLN("[INFO soft_ap] Starting Access point mode.\n");
-
 	state = STATE_ACCESS_POINT_ACTIVE;
-
 	teardownWifi();
-
 	WiFi.mode(WIFI_AP);
-
 	WiFi.softAP( _ap_ssid, _ap_password );
-
 	DEBUG_PRINTLN("[INFO soft_ap] Access Point IP address is : %s.\n", &((WiFi.softAPIP().toString())[0]));
-
 	server->on(BOOTLITE_CONFIG_URI, HTTP_GET, [this](){ handleConfig(); });
-
 	server->onNotFound([this](){ handleNotFound(); });
-
 	server->begin();
-
 	DEBUG_PRINTLN("[soft_ap] Returning from function.\n");
-
 	return NO_ERROR;
 }
 
@@ -392,59 +273,38 @@ ESPBootstrapError ESP8266BootstrapLite::startSoftAP() {
 * a new entry.
 */
 void ESP8266BootstrapLite::storeWifiConfInSPIFF(String ssid, String password) const{
-
 	//ToDo : Check available size of SPIFF
-
 	if(SPIFFS.begin()){
-
 	  if(SPIFFS.exists(BOOTLITE_SAVED_NETCONFIGS_FILE)){
-
 		 File f = SPIFFS.open(BOOTLITE_SAVED_NETCONFIGS_FILE, "w+");
 
 		  if(f){
-				
 			bool found = false;
-
 			while(f.available()){
-
 			  if(ssid == f.readStringUntil('\n')){
-						
 				f.println(password);
-
 				DEBUG_PRINTLN("[INFO store_wifi] SSID %s already exists. Overwriting password for ssid.\n", &ssid[0]);
-						
 				found = true;
-						
 				break;
-					
 			  }else{
-
 				String pass = f.readStringUntil('\n');
 			  }
-
 			}
 				
 			if(!found){
-					
 			  f.println(ssid);
 			  f.println(password);
-
 			  DEBUG_PRINTLN("[INFO store_wifi] configuration saved.\n");
 			}
 
-			 f.close();
-				
+			f.close();
 		  }else{
-
 			DEBUG_PRINTLN("[INFO store_wifi] File error. Cannot store the Wifi credentials.\n");
 		  }
 	  } else{
-
 		DEBUG_PRINTLN("[INFO store_wifi] Config file not found.\n");
 	  }
-
 	} else {
-
 		DEBUG_PRINTLN("[INFO store_wifi] SPIFFS did not mount correctly. Cannot store the Wifi credentials.\n");
 	}
 }
@@ -454,78 +314,46 @@ void ESP8266BootstrapLite::storeWifiConfInSPIFF(String ssid, String password) co
 * Return an error in case it fails to connect.
 */
 ESPBootstrapError ESP8266BootstrapLite::connectToWifi(String ssid, String password){
-
-	
 	if( (ssid == "\0" || ssid.length() == 0) || (password == "\0" || password.length() == 0) ){ 
-
 		DEBUG_PRINTLN("[ERROR wifi_connect] Either ssid or password is empty.\n");
-
 		return ERROR_WIFI_CONNECT;
 	}
 
 	teardownWifi();
-
 	WiFi.mode(WIFI_STA);
-
 	WiFi.begin(&ssid[0], &password[0]);
-
 	DEBUG_PRINTLN("[INFO wifi_connect] Connecting to Wifi network with ssid = %s.\n", &ssid[0]);
 
 	while(WiFi.status() != WL_CONNECTED){
-
 		bool _connected = false;
-
 		delay(BOOTLITE_DELAY_TIME * 5);
-
 		switch(WiFi.status()){
-
 			case WL_CONNECTED: 
-
 				_connected = true;
-
-			break;
-
+				break;
 			case WL_NO_SSID_AVAIL:
-
 				DEBUG_PRINTLN("[ERROR wifi_connect] No SSID available. Please try again.\n");
-
 				return ERROR_WIFI_CONNECT;
-
-			break;
-
+				break;
 			case WL_CONNECT_FAILED:
-
 				DEBUG_PRINTLN("[ERROR wifi_connect] Incorrect password. Please try again.\n");
-
 				return ERROR_WIFI_CONNECT;
-
-			break;
-
+				break;
 			case WL_DISCONNECTED:
-
 				DEBUG_PRINTLN("[ERROR wifi_connect] Could not connect to Wifi. Either password is incorrect or Wifi router is OFF/Out-of-Range.\n");
-
 				return ERROR_WIFI_CONNECT;
-
-			break;
-
+				break;
 			default:
-
 				DEBUG_PRINTLN(".");
-
-			break;
-
+				break;
 		}
 
 		if(_connected) break;
 	}
 
 	DEBUG_PRINTLN("[INFO wifi_connect] Connection successful.\n");
-
 	DEBUG_PRINTLN("[INFO wifi_connect] IPAddress assigned to this device is %s.\n", &((WiFi.localIP().toString())[0]));
-
 	state = STATE_WIFI_ACTIVE;
-
 	return NO_ERROR;
 }
 
@@ -557,17 +385,11 @@ void ESP8266BootstrapLite::setState(ESPBootstrapState state){ this->state = stat
 * Helper method that disconnects from the current wifi settings.
 */
 void ESP8266BootstrapLite::teardownWifi(){
-
 	DEBUG_PRINTLN("[INFO teardown] Shutting down wifi and restarting it.\n");
-
 	delay(BOOTLITE_DELAY_TIME);
-
 	if(WiFi.isConnected()){
-
 	    WiFi.softAPdisconnect();
-
 	    WiFi.disconnect();
-
 	    delay(BOOTLITE_DELAY_TIME);
   	}
 }
@@ -578,13 +400,10 @@ void ESP8266BootstrapLite::teardownWifi(){
 */
 
 void ESP8266BootstrapLite::enableOTAUpdates(const String apihost, const String port, String userKey){
-  
  	DEBUG_PRINTLN("[INFO OTA update] Enabling OTA update for the device.\n");
-
  	_ota_enabled = true;
 
  	if( userKey == "\0" || userKey.length() <= 6) {
-
  		userKey = getUserTokenFromSPIFFS();
  	}
 
@@ -594,9 +413,7 @@ void ESP8266BootstrapLite::enableOTAUpdates(const String apihost, const String p
 }
 
 void ESP8266BootstrapLite::disableOTAUpdates(){
-	
 	DEBUG_PRINTLN("[INFO OTA update] Disabling OTA update for the device.\n");
-	
 	_ota_enabled = false;
 }
 
@@ -609,16 +426,13 @@ ESPBootstrapError ESP8266BootstrapLite::update(String macAddress){
 	OTAError err;
 
 	if(_ota_enabled){
-		
 		err =  ota.performUpdate(macAddress);
-
 		if(err == OTA_NO_ERROR){
 			return NO_ERROR;
 		}
 	}
 
 	DEBUG_PRINTLN("[INFO OTA update] OTA update was disabled.");
-
 	return ERROR_OTA_UPDATE_FAILED;
 }
 
--- EspModules/ESP8266BootstrapLite/src/ESP8266OTAUpdate.cpp
+++ EspModules/ESP8266BootstrapLite/src/ESP8266OTAUpdate.cpp
@@ -1,8 +1,6 @@
 
 #include "ESP8266OTAUpdate.h"
 
-
-
 #define GET_DEVICE_INFO_API_ENDPOINT "api/device/mac"
 
 #define UPDATE_API_ENDPOINT "api/firmware/ota/update/device/"
@@ -20,33 +18,23 @@
 ESP8266OTAUpdate::ESP8266OTAUpdate(String apihost, String port, String userToken) {
 
 	if(apihost != "\0" && apihost.length() != 0) {
-
 		url = apihost;
-
 		if(port != "\0" && port.length() != 0) {
-
 			url = "http://" + url + ":" + port + "/";
 		}
-
 	}else{
-
 		DEBUG_PRINTLN("API host and port not provided. Using default 'http://localhost:3000/' as api host.\n");
-
 		url = DEFAULT_URL;
 	}
-
 	if(userToken == "\0" || userToken.length() <= 6) {
 		zombie = true;
-
 	} else {
 		user_token = userToken;
-
 	}
 }
 
 
 ESP8266OTAUpdate::ESP8266OTAUpdate(String apihost, String userToken){
-
 	ESP8266OTAUpdate(apihost, "\0", userToken);
 }
 
@@ -55,41 +43,26 @@ ESP8266OTAUpdate::ESP8266OTAUpdate(String apihost, String userToken){
 *  Helper function that retreives info from the device's permanent memory i.e. SPIFFS.
 */
 String ESP8266OTAUpdate::getInfoFromSPIFFS(String key){
-
 	if(zombie) {
-
 		DEBUG_PRINTLN("Object creation failed as user token was not provded.\n");
-		
 		return "\0";
 	}
 
-
 	if(SPIFFS.begin() && SPIFFS.exists(FILE_DEVICE_IDENTITY)){
-
 	  File f = SPIFFS.open(FILE_DEVICE_IDENTITY, "r");
-
 	  if(f){
-
 		String key = f.readStringUntil('\n');
-
 		if(key && key.length() > 0){
-
 		  String deviceId = f.readStringUntil('\n');
-
 		  DEBUG_PRINTLN("Device Id stored = %s.\n", &deviceId[0]);
-
 		  deviceId = deviceId.substring(0, deviceId.length()-1);
-
 		  if(deviceId != "\0" && deviceId.length() > 6){
-
 			f.close();
-
 			return deviceId;
 		  }
 		}
 	  }
 	}
-
 	return "\0";
 }
 
@@ -97,15 +70,11 @@ String ESP8266OTAUpdate::getInfoFromSPIFFS(String key){
 *  Helper function that stores info on the device's permanent memory i.e. SPIFFS.
 */
 void ESP8266OTAUpdate::storeInfoInSPIFFS(String key, String value){
-
 	if(zombie) {
-
 		DEBUG_PRINTLN("Object creation failed as user token was not provded.\n");
 	} else {
-
 		File f = SPIFFS.open(FILE_DEVICE_IDENTITY, "w+");
-
-		if(f){
+		if(f) {
 		  f.println(key);
 		  f.println(value);
 		  f.close();
@@ -120,58 +89,40 @@ void ESP8266OTAUpdate::storeInfoInSPIFFS(String key, String value){
 *  If "resourceUri" is not provided then it tries to access a default uri.
 */
 String ESP8266OTAUpdate::getDeviceIndentityFromServer(String macAddress, String resourceUri){
-
 	if(zombie) {
-
 		DEBUG_PRINTLN("Object creation failed as user token was not provded.\n");
-
 		return "\0";
 	}
 
 	HTTPClient http;
-
 	StaticJsonBuffer<1500> JSONBuffer;
 
 	if(WiFi.status() == WL_CONNECTED){
-
 		if(resourceUri != NULL && resourceUri.length() != 0){
-
 			http.begin(url + resourceUri + "?macaddress=" + macAddress);
-
 		} else {
-
 			http.begin(url + GET_DEVICE_INFO_API_ENDPOINT + "?macaddress=" + macAddress);
 		}
 
 		http.addHeader("Authentication", user_token);
-
 		int httpCode = http.GET();
 
 		if(httpCode > 0){
-
 			String payload = http.getString();
-
 			JsonObject& parsed = JSONBuffer.parseObject(payload);
 
 			if(!parsed["message"]){
-
 				http.end();
-
 				String result;
-
 				result = String(parsed["_id"].as<const char*>());
-
 				return result;
 			}
 
 			http.end();
-
 		} else{
-			
 			DEBUG_PRINTLN("Get response with server code : %d.\n", httpCode);
 		}
 	}
-
 	return "\0";
 }
 
@@ -180,13 +131,9 @@ String ESP8266OTAUpdate::getDeviceIndentityFromServer(String macAddress, String 
 *  Performs update on the device by communicating with the server. It is a helper function called by the public method "performUpdate"
 */
 OTAError ESP8266OTAUpdate::update(String deviceId){
-
 	HTTPClient http;
-
 	http.begin(url + UPDATE_API_ENDPOINT + deviceId);
-
 	http.addHeader("Authentication", user_token);
-
 	HTTPUpdateResult ret = handleUpdate(http, "\0");
 
 	switch(ret) {
@@ -194,16 +141,13 @@ OTAError ESP8266OTAUpdate::update(String deviceId){
           DEBUG_PRINTLN("HTTP_UPDATE_FAILD Error (%d): %s.\n", ESPhttpUpdate.getLastError(), 
           						&((ESPhttpUpdate.getLastErrorString().c_str())[0]));
           break;
-
       case HTTP_UPDATE_NO_UPDATES:
           DEBUG_PRINTLN("HTTP_UPDATE_NO_UPDATES.\n");
           break;
-
       case HTTP_UPDATE_OK:
           DEBUG_PRINTLN("HTTP_UPDATE_OK.\n");
           break;
     }
-
     return OTA_UPDATE_FAILED;
 }
 
@@ -211,40 +155,28 @@ OTAError ESP8266OTAUpdate::update(String deviceId){
 *  User should call this method to download new firmware.
 */
 OTAError ESP8266OTAUpdate::performUpdate(String macAddress){
-
 	if(zombie) {
-
 		DEBUG_PRINTLN("Object creation failed as user token was not provided.\n");
-		
 		return OTA_UNINDENTIFIED_USER;
 	}
-
 
 	String deviceId = getInfoFromSPIFFS(DEVICE_IDENTITY);
 
 	if(deviceId == NULL){
-
 		if(WiFi.status() != WL_CONNECTED) {
-
 			DEBUG_PRINTLN("No network connection.\n");
-
 			return OTA_NO_NETWORK_CONNECTION;
 		}
 
 		deviceId = getDeviceIndentityFromServer(macAddress, GET_DEVICE_INFO_API_ENDPOINT);
 
 		if(deviceId == NULL) {
-
 			DEBUG_PRINTLN("Device could not be identified.\n");
-
 			return OTA_UNINDENTIFIED_DEVICE;
-
 		} else {
-
 			storeInfoInSPIFFS(DEVICE_IDENTITY, deviceId);
 		}
 	}
-
 	return update(deviceId);
 }
 
